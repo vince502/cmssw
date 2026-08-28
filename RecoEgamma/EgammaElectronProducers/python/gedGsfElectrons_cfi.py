640eb8e55336ce1b5a44db7b524fc4e8557ffe74
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2026-08-28 16:32:45 +0200

    Remove extra ECAL SC cut for electrons in PbPb era

--- RecoEgamma/EgammaElectronProducers/python/gedGsfElectrons_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/gedGsfElectrons_cfi.py
@@ -43,10 +43,6 @@ gedGsfElectronsTmp = ecalDrivenGsfElectrons.clone(
 
 
 
-from Configuration.ProcessModifiers.pp_on_AA_cff import pp_on_AA
-pp_on_AA.toModify(gedGsfElectronsTmp.preselection, minSCEtBarrel = 15.0)
-pp_on_AA.toModify(gedGsfElectronsTmp.preselection, minSCEtEndcaps = 15.0)
-
 from Configuration.ProcessModifiers.egamma_lowPt_exclusive_cff import egamma_lowPt_exclusive
 egamma_lowPt_exclusive.toModify(gedGsfElectronsTmp.preselection,
                                 minSCEtBarrel = 1.0, 
